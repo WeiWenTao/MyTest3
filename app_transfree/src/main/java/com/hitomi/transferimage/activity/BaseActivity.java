77dff8bdf5d28368cf236d07df6e11a21ceff539
WWT <WWT>
2017-05-20 09:42:57 +0800

    注释语句

---
+++ app_transfree/src/main/java/com/hitomi/transferimage/activity/BaseActivity.java
@@ -0,0 +1,62 @@
+package com.hitomi.transferimage.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.hitomi.tilibrary.transfer.Transferee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hitomi on 2017/2/13.
+ */
+
+public abstract class BaseActivity extends Activity {
+
+    protected Transferee transferee;
+    protected GridView gvImages;
+    protected List<String> thumbnailImageList;
+    protected List<String> sourceImageList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        transferee = Transferee.getDefault(this);
+        setContentView(getContentView());
+        initView();
+        testTransferee();
+    }
+
+    /**
+     * 包装缩略图 ImageView 集合
+     *
+     * @return
+     */
+    @NonNull
+    protected List<ImageView> wrapOriginImageViewList(int size) {
+        List<ImageView> originImgList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            ImageView thumImg = (ImageView) ((LinearLayout) gvImages.getChildAt(i)).getChildAt(0);
+            originImgList.add(thumImg);
+        }
+        return originImgList;
+    }
+
+    protected abstract int getContentView();
+
+    protected abstract void initView();
+
+    protected abstract void testTransferee();
+
+    @Override
+    protected void onDestroy() {
+        transferee.destroy();
+        super.onDestroy();
+    }
+}
